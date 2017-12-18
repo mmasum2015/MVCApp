3dec8e336936a4a3a6f160bf324a260171ac7ca8
Mohammad Masum <masum2masum@gmail.com>
2017-12-18 12:18:37 +0100

    Add project files.

---
+++ AdressBook/Data/Migrations/20171218100841_init.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AdressBook.Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Persons",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Adress",
+                table: "Persons",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Persons",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Adress",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Persons");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Persons",
+                newName: "Name");
+        }
+    }
+}
---
+++ AdressBook/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdressBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
